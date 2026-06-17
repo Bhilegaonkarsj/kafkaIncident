4dc0841c2205cca6f3f221d0c3c46c6109b93cb8
Sakshi Bhilegaonkar <sakshi.bhilegaonkar@locationguru.com>
2026-06-17 13:37:36 +0530

    Email processed

--- src/main/java/com/incident/consumer/NotificationConsumer.java
+++ src/main/java/com/incident/consumer/NotificationConsumer.java
@@ -1,19 +1,33 @@
 package com.incident.consumer;
 
 import com.incident.dto.IncidentCreatedEvent;
+import com.incident.service.EmailService;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
-public class NotificationConsumer
-{
+@Component
+public class NotificationConsumer {
+
+	private final EmailService emailService;
+
+	public NotificationConsumer(EmailService emailService) {
+		this.emailService = emailService;
+	}
 
 	@KafkaListener(
 			topics = "incident-created",
 			groupId = "notification-group"
 	)
-	public void consume(IncidentCreatedEvent event)
-	{
-		System.out.println("Notification received -> " + event.incidentName());
+	public void consume(IncidentCreatedEvent event) {
+
+		System.out.println("Received Event : "
+		                   + event.incidentName());
+
+		emailService.sendEmail(
+				"bhilegaonkarsakshi@gmail.com",
+				"New Incident Created",
+				"Incident Created : "
+				+ event.incidentName()
+		);
 	}
 }
